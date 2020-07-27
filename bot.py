49a50a94e084d6a57c0916a458a87863672be928
5191037 <5191037@st.hsc.ac.jp>
2020-07-27 13:11:38 +0900

    gitignore

---
+++ bot.py
@@ -0,0 +1,67 @@
+import time
+import re
+import requests
+import sys
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+from tqdm import tqdm
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
+def kamigame(URL):
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get(URL)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.close()
+    driver.quit()
+
+    site_info = soup.select('#top_mobile_game_list')
+    mobile = site_info[0].find_all('a')
+
+    for info in tqdm(mobile):
+        name = info.get_text()
+        url = ("https://kamigame.jp" + info.get('href'))
+        # print(name, url + '\n')
+        my_dict = {'name': name, 'url': url}
+        collection.insert_one(my_dict)
+
+    site_info = soup.select('#top_consumer_game_list')
+    consumer = site_info[0].find_all('a')
+
+    for info in tqdm(consumer):
+        name = info.get_text()
+        url = ("https://kamigame.jp" + info.get('href'))
+        # print(name, url + '\n')
+        my_dict = {'name': name, 'url': url}
+        collection.insert_one(my_dict)
+
+
+if __name__ == "__main__":
+    args = sys.argv
+
+    if len(args) != 2:
+        print("Usage:python bot.py URL")
+        sys.exit(1)
+
+    URL = args[1]
+    p = re.compile(r'^(http|https)://[a-z0-9][a-z0-9\-\._]*\.[a-z]+'r'(?:[0-9]+)?(?:/.*)?$')
+
+    if not p.match(URL):
+        print("invaid argument.")
+        sys.exit(2)
+
+    client = MongoClient('localhost', 27017)
+    db = client.scraping
+    collection = db.bot_fe
+
+    kamigame(URL)
