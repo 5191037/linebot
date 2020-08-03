47171122a58512eb997454be8fde486af39edb2a
5191037 <5191037@st.hsc.ac.jp>
2020-08-03 13:33:13 +0900

    .drone.yml

--- app.py
+++ app.py
@@ -76,6 +76,10 @@ def callback():
         db = client.scraping
         collection = db.bot_fe
 
+        if event.message.text == "【News】":
+            for record in collection.find(filter={'name': {'$regex': event.message.text}}):
+                event.message.text += record["name"] + record["url"] + "\n"
+
         if event.message.text == "【Search】":
             hard_list = ["五十音順", "シリーズ"]
             items = [QuickReplyButton(action=MessageAction(label=f"{hard}", text=f"{hard}")) for hard in hard_list]
@@ -156,10 +160,6 @@ def callback():
             items = [QuickReplyButton(action=MessageAction(label=f"{hard}", text=f"{hard}")) for hard in hard_list]
             messages = TextSendMessage(text="シリーズ", quick_reply=QuickReply(items=items))
             line_bot_api.reply_message(event.reply_token, messages=messages)
-
-        if event.message.text == "【News】":
-            for record in collection.find(filter={'name': {'$regex': event.message.text}}):
-                event.message.text += record["name"] + record["url"] + "\n"
 
         return_text = ""
         for record in collection.find(filter={'name': {'$regex': event.message.text}}):
